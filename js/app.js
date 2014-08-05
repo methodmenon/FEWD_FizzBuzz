81651cbb5ebe797f799f2059933fa6382096f338
methodmenon <methodmenon@gmail.com>
2014-08-04 18:30:08 -0700

    Preliminary Fizz Buzz Code

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$("#btn_val").click(function(){
+		fB_val = fizzBuzz($("#val").val());
+		fB_li = $("<li></li>").text(fB_val);
+		$("#fb_list").append(fB_li);
+	});
+
+});
+
+function fizzBuzz(x) {
+
+	for (var i = x; i < 100; i++) {
+		if ((i % 3) == 0 && ((i % 5) == 0)) {
+			console.log("fizz buzz");
+		}
+		else if ((i % 3) == 0) {
+			console.log("fizz");
+		}
+		else if ((i % 5) == 0) {
+			console.log("buzz");
+		}
+		else
+		{
+			console.log(i);
+		}
+	}
+}
